b4cd26e029887dafec874c8a5d16692d29a5235e
iohub <rickyang.pro@gmail.com>
2024-04-27 17:33:41 +0800

    update to wrk2

--- wrk_bench.sh
+++ wrk_bench.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-wrk -t 8 -c 80 -d 60s -s wrk/vllm.lua -v http://127.0.0.1:8000/v1/completions
+wrk2 -t 8 -c 80 -d 60s http://127.0.0.1:8000/v1/completions -L -U -s wrk/llamacpp.lua -R 100
+
