488b0ad186a75c4893987a2368cf5baaf5f78e42
iohub <rickyang.pro@gmail.com>
2024-04-27 14:01:50 +0800

    wrk benchmark

--- llamacpp/api.sh
+++ llamacpp/api.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 bin/server -ngl 81 -t 12 \
     -m /home/do/ssd/modelhub/llama-2-7b-hf/llama2-7b.f16.bin \
-    -c 2048 -np 8 --host 0.0.0.0 --port 8200 
+    -c 2048 -np 2 --host 0.0.0.0 --port 8200 \
+    -b 2048 -ub 512
