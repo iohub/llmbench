52eca8884e5e52645add374b63003ec75dd373d2
iohub <rickyang.pro@gmail.com>
2024-04-27 17:14:21 +0800

    print response

--- wrk/tllm.lua
+++ wrk/tllm.lua
@@ -17,3 +17,13 @@ local body = JSON:encode(data)
 print(body)
 
 wrk.body = body
+
+
+-- 响应处理函数
+response = function(status, headers, body)
+    -- 打印HTTP状态码
+    print("Status: " .. status)
+
+    -- 打印响应体
+    print("Body: " .. body)
+end
